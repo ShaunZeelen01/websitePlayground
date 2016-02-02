0119203590e960f299bef40478962d7544e3a011
Christopher <captainless2@gmail.com>
2016-02-02 15:48:22 +0000

    Made a weird animation

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $(".box").click(function(){
         $(".box").animate({height:"300px"});
+        $(".box").animate({width:"300px"});
+        $(".box").animate({height:"100px"});
+        $(".box").animate({width:"100px"});
     });
 });
