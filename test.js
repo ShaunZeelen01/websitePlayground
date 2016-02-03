3f3ad295615d9d93306d45b1d76faba78e458e2b
Christopher <captainless2@gmail.com>
2016-02-03 16:45:10 +0000

    Added moving boxes

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     $(".box").click(function(){
-        $(".box").animate({height:"300px"});
-        $(".box").animate({width:"300px"});
-        $(".box").animate({height:"100px"});
-        $(".box").animate({width:"100px"});
-    });
+        $("#Jon").animate({left: '+=130px', top:"+=120px"});
+        $("#Jack").animate({left: '-=130px', top:"+=120px"});
+        $("#Jim").animate({left: '+=130px', top:"-=120px"});
+        $("#Sen").animate({left: '-=130px', top:"-=120px"});
+    });    
 });
+
